923d3bad3d80631f0dd04506ff020921714c543b
Huang Chengwei <primecriminalkid@gmail.com>
2013-01-31 10:49:16 +0800

    Validation framework added
    This is a simple request arguments validation framework, inspired by
    Struts2.

---
+++ __init__.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2012 Kid143
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# This is a customed blog different from demo
+
+'''Miscellous tools here.'''
+
+import functools
+
+def validate(field_config):
+    '''This decorator is used for validating request parameters.
+       Its design is inspired by the Annotated Validation framework
+       from Struts2 in Java. You can write your own validator function
+       to meet your own project requirements. For more info on 
+       customized validator function, please refer to the docstring in
+       validators module.
+    '''
+    def _validate(http_method_func):
+        @functools.wraps(http_method_func)
+        def __validate(self, *args, **kwargs):
+            arguments = self.request.arguments
+            self.logger.info('Validating requests...')
+            error_info = []
+            for name in field_config.keys():
+                values = arguments.get(name, [])
+                if len(values) > 0:
+                    validators = field_config.get(name, [])
+                    for value in values:
+                        for validator in validators:
+                            valid_state = None
+                            try:
+                                valid_state = validator[0](value, *validator[1])
+                            except TypeError:
+                                #if validator arguments are not provided
+                                valid_state = validator(value)
+                            if isinstance(valid_state, tuple):
+                                assert isinstance(valid_state[1], str)
+                                error_info.append({
+                                            'fieldName': name,
+                                            'value': value,
+                                            'error_message': valid_state[1]
+                                        })
+            
+            # You need a customized error page to output error info
+            # This part can be modifed according to your requirements.
+            if error_info:
+                error_output = ''
+                for error in error_info:
+                    for key, value in error.items():
+                        error_output += key
+                        error_output += ': '
+                        error_output += value
+                        error_output += '<br/>'
+
+                    error_output += '<br/>'
+
+                self.dataModel={
+                        'status_code': 400,
+                        'err_info': error_output
+                        }
+                self.render('error.html', **self.dataModel)
+                return
+
+            #must use return so the function do not block in asynchronous mode.
+            return http_method_func(self, *args, **kwargs)
+        return __validate
+    return _validate
---
+++ validators.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2012 Kid143
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# This is a customed blog different from demo
+
+'''Validators used for http request parameter validation.
+   Validators are functions that validate request arguments.
+   You can follow the instructions below to write your own validator:
+
+   1. Validator function arguments begin with "value", which represent
+      the request argument value, the following arguments are for the
+      validation progress, which has no special requirements.
+   2. There must be a message_info string in the arguments, which is
+      used for error output.
+   3. If the request argument value is valid, just return True, otherwise
+      return a tuple (False, message_info)
+'''
+
+import re
+
+def regex_validator(value, regex, message_info):
+    '''use regex to test whether the value is matched'''
+    matched = bool(re.match(regex, value, re.VERBOSE))
+    
+    return True if matched else (False, message_info)
+
+def length_validator(value, length, mode ,message_info):
+    '''validate the length of the parameter value.'''
+    if mode == "longer":
+        return True if len(value) >= length else (False, message_info)
+    elif mode == "nolonger":
+        return True if len(value) <= length else (False, message_info)
+    else:
+        print('Warning: mode not recognized, no validation has done.')
+        return True
+
+def email_validator(value, message_info='email address form not match'):
+    '''validate email address.'''
+    matched = bool(re.match(
+        r'^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$', 
+        value, re.VERBOSE))
+
+    return True if matched else (False, message_info)
+
+def required_validator(value, message_info='missing arguments.'):
+    return True if not value or not value.strip() else (False, message_info)
